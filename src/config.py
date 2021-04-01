8d42b2060a24d224c4a9d8dc64be4cb2cd28ab0b
David Reynolds <david@reynoldsfamily.org.uk>
2021-04-01 08:33:25 +0100

    Attempt fix typing

--- src/config.py
+++ src/config.py
@@ -1,6 +1,7 @@
 import json
 import logging
 from pathlib import Path
+from typing import Union
 
 import jsonschema
 
@@ -14,14 +15,14 @@ class UrlConfigFile:
         self.file_name = kwargs.pop("file_name")
         self.schema = kwargs.pop("schema", CONFIG_SCHEMA)
 
-    def load_file(self, file_name: str):
+    def load_file(self, file_name: str) -> Union[Path, None]:
         config_file = Path(file_name)
         if not config_file.exists() or not config_file.is_file():
             LOG.debug(f'"{self.file_name}" does not exist')
             return None
         return config_file
 
-    def validate_config(self, config_data: str) -> dict:
+    def validate_config(self, config_data: str) -> Union[dict, None]:
         # Validate config schema
         try:
             jsonschema.validate(instance=config_data, schema=CONFIG_SCHEMA)
@@ -30,7 +31,7 @@ class UrlConfigFile:
             config_data = None
         return config_data
 
-    def get_config(self) -> str:
+    def get_config(self) -> Union[str, None]:
         config_file = self.load_file(self.file_name)
         try:
             config_data = json.load(config_file.open())
